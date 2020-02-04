e7ca2255a2b13033ec4ef8302e0979ac8b229d02
Heedee87 <hidenori.kawano@gmail.com>
2020-02-04 12:44:51 +0000

    completed hash table exercises, created folders for each sections

---
+++ hash/recurring.js
@@ -0,0 +1,73 @@
+//Google Question
+//Given an array = [2,5,1,2,3,5,1,2,4]:
+//It should return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4]:
+//It should return 1
+
+//Given an array = [2,3,4,5]:
+//It should return undefined
+
+
+function firstRecurringCharacter(input) {
+  const checkedNum = []
+  for(let i=0;i<input.length;i++){
+    if(!checkedNum.includes(input[i])){
+      checkedNum.push(input[i])
+    }else{
+      return input[i]
+    }
+  }
+  return undefined
+}
+
+function firstRecurringCharacter2(input){
+  const checkedNum = new Map()
+
+  for(let i=0; i < input.length; i++){
+    if(checkedNum.has(input[i])){
+      return input[i]
+    }else{
+      checkedNum.set(input[i], i)
+    }
+  }
+  return undefined
+}
+
+
+
+//Bonus... What if we had this:
+// [2,5,5,2,3,5,1,2,4]
+// return 5 because the pairs are before 2,2
+
+//----Solution Example----
+function firstRecurringCharacterS1(input){
+  for(let i=0; i < input.length; i++){
+    for(let j=i+1; j < input.length; j++){
+      if(input[i] === input[j]){
+        return input[i]
+      }
+    }
+  }
+  return undefined
+}
+
+function firstRecurringCharacterS2(input){
+  let map = {}
+  for(let i=0; i < input.length; i++){
+    console.log(map)
+    if(map[input[i]] !== undefined){
+      return input[i]
+    }else {
+      map[input[i]] = i
+    }
+  }
+  return undefined
+}
+
+module.exports = {
+  firstRecurringCharacter,
+  firstRecurringCharacter2,
+  firstRecurringCharacterS1,
+  firstRecurringCharacterS2
+}
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const MyArray = require("./array/Array")
+//----Arrays----
+
+const MyArray = require("./arrays/arrays")
 
 // const newArray = new MyArray();
 // newArray.push("hi");
@@ -12,10 +14,25 @@ const MyArray = require("./array/Array")
 // newArray.delete(1);
 // console.log(newArray);
 
-const {reverse} = require("./reverse/reverse")
+const {reverse} = require("./arrays/reverse")
 // console.log(reverse("Hi My name is Andrei"));
 
-
-
-const {mergeSorterdArrays, mergeSorterdArrays1} = require("./merge/merge");
+const {mergeSorterdArrays, mergeSorterdArrays1} = require("./arrays/merge");
 mergeSorterdArrays1([0,3,4,31],[4,6,30])
+
+
+//----Hash-----
+const HashTable = require("./hash/hash")
+
+const myHashTable = new HashTable(50);
+myHashTable.set('grapes', 10000)
+myHashTable.set('apples', 54)
+myHashTable.set('oranges', 2)
+//myHashTable.get('apples')
+myHashTable.keys()
+
+
+const {firstRecurringCharacter,firstRecurringCharacter2} = require("./hash/recurring")
+
+firstRecurringCharacter2([2,1,1,2,3,5,1,2,4])
+firstRecurringCharacter2([2,3,4,5])

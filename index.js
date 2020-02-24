3d9a7a55882990b63cc72018e81700e345b98720
hideparedes <hidenori.kawano@gmail.com>
2020-02-24 07:05:25 +0000

    Data Structures: Stacks + Queues

--- index.js
+++ index.js
@@ -42,10 +42,57 @@ firstRecurringCharacter2([2,3,4,5])
 const LinkedList = require("./linked-list/linked.list")
 
 const myLinkedList = new LinkedList(10)
-myLinkedList.append(5)
-myLinkedList.append(16)
-myLinkedList.prepend(4)
-myLinkedList.insert(2,99)
-myLinkedList.insert(24,88)
-myLinkedList.remove(3)
-myLinkedList.printList()
+// myLinkedList.append(5)
+// myLinkedList.append(16)
+// myLinkedList.prepend(4)
+// myLinkedList.insert(2,99)
+// myLinkedList.insert(24,88)
+// myLinkedList.remove(3)
+// myLinkedList.reverse()
+// myLinkedList.printList()
+
+const DoublyLinkedList = require("./linked-list/doubly.linked.js")
+
+// const myDoublyLinkedList = new DoublyLinkedList(10);
+// myDoublyLinkedList.append(5);
+// myDoublyLinkedList.append(16);myDoublyLinkedList.prepend(1);
+// myDoublyLinkedList.printList()
+// myDoublyLinkedList.insert(2, 99);
+// // myDoublyLinkedList.insert(20, 88);
+// myDoublyLinkedList.remove(2);
+//myDoublyLinkedList.printList()
+
+
+const StackL = require("./stack/stack-linked-list")
+
+const linkStack = new StackL();
+
+// linkStack.push("google");
+// linkStack.push("YouTube");
+// console.log(linkStack.push("Facebook"));
+// linkStack.pop();
+// linkStack.pop();
+// console.log(linkStack.pop());
+
+const StackA = require("./stack/stack-array");
+
+const arrayStack = new StackA();
+
+// arrayStack.push("google");
+// arrayStack.push("YouTube");
+// console.log(arrayStack.push("Facebook"));
+// console.log(arrayStack.peek());
+// console.log(arrayStack.pop());
+
+const Queue = require("./queue/queue");
+
+const myQueue = new Queue;
+
+myQueue.enqueue("Joy");
+myQueue.enqueue("Matt");
+//console.log(myQueue.peek());
+myQueue.enqueue("Pavel");
+console.log(myQueue.enqueue("Samir"))
+console.log(myQueue.dequeue());
+console.log(myQueue.peek());
+
---
+++ linked-list/doubly.linked.js
@@ -0,0 +1,91 @@
+// add a method remove() to the linked list that deletes a node to the specified index.
+
+class DoublyLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+      prev: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+      prev: null
+    }
+    newNode.prev = this.tail
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: null,
+      prev: null
+    }
+    newNode.next = this.head;
+    this.head.prev = newNode
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while(currentNode !== null){
+        array.push(currentNode.value)
+        currentNode = currentNode.next
+    }
+    console.log(array)
+    return array;
+  }
+  insert(index, value){
+    //Check for proper parameters;
+    if(index >= this.length) {
+      console.log('yes')
+      return this.append(value);
+    }
+    
+    const newNode = {
+      value: value,
+      next: null,
+      prev: null
+    }
+    const leader = this.traverseToIndex(index-1);
+    const follower = leader.next;
+    leader.next = newNode;
+    newNode.prev = leader
+    newNode.next = follower;
+    follower.prev = newNode
+    this.length++;
+    return this.printList();
+  }
+  traverseToIndex(index) {
+    //Check parameters
+    let counter = 0;
+    let currentNode = this.head;
+    while(counter !== index){
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  remove(index) {
+    // Check Parameters      
+    const leader = this.traverseToIndex(index-1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    const follower = leader.next
+    follower.prev = leader 
+    this.length--;
+    return this.printList();
+  }
+}
+
+module.exports = DoublyLinkedList
+
